f8a53e7810f35939d1a946390129bdcf49813bca
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 15:45:39 +0000

    Switch

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,17 @@
 public class Main {
     
-
-    private static int x = 2;
-    private static int y = 5;
-    
-    private static int a = 3;
-    private static int b = 7;
-    private static int c = 11;
-    
-    private static int year = 2019;
-    
     /**
-     * Operatorpraezedenz
+     * Switch
      */
     public static void main(String[] args){
         
-        int z = ++x * y--;
-        System.out.println(z);
-        
-        int result = a-++c-++b;
-        //System.out.println(result);
-              
-        boolean schaltjahr = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-        //System.out.println(schaltjahr);	
-        
-        //System.out.println(1 + 2 + "abc");
-        //System.out.println("abc" + 1 + 2);
+        int wochentag = 5;
+
+        switch (wochentag){
+            case 1: System.out.println("vormittags");
+            case 2: case 3: case 4: case 5:
+                System.out.println("geöffnet"); break;
+            default: System.out.println("geschlossen");
+        }
     }
-    
 }
