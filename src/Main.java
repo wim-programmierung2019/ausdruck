b457b473a44633bbd8c0c946aca5688cae07c4c1
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 15:27:53 +0000

    Übungsaufgabe

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    
+    /**
+     * Einstieg in ein Javaprogramm.
+     */
+    public static void main(String[] args){
+        // deklariere eine ganzzahlige Variable "birnen"
+		
+		// weise ihr den Wert drei zu
+
+		System.out.println("3 erwartet: " + birnen);
+		// addiere fuenf zu dem Wert
+
+		System.out.println("8 erwartet: " + birnen);
+		// deklariere eine Variable "aepfel" und weise ihr den Wert fuenf zu
+
+		System.out.println("5 erwartet: " + aepfel);
+		// subtrahiere zwei von dem Wert
+
+		System.out.println("3 erwartet: " + aepfel);
+
+    }
+    
+}
