a8b3599104b4c6a145fd1de4c0b78783e3d977fd
friej <julian-frie@gmx.de>
2019-07-19 12:15:23 +0000

    Übung

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,23 @@ public class Main {
      */
     public static void main(String[] args){
         // deklariere eine ganzzahlige Variable "birnen"
+        int birnen;
 		
 		// weise ihr den Wert drei zu
+		birnen = 3;
 
 		System.out.println("3 erwartet: " + birnen);
 		// addiere fuenf zu dem Wert
+		birnen = birnen + 5;
+	
 
 		System.out.println("8 erwartet: " + birnen);
 		// deklariere eine Variable "aepfel" und weise ihr den Wert fuenf zu
+		int aepfel = 5;
 
 		System.out.println("5 erwartet: " + aepfel);
 		// subtrahiere zwei von dem Wert
+		aepfel = aepfel - 2;
 
 		System.out.println("3 erwartet: " + aepfel);
 
