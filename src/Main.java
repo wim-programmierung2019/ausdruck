40bc456934dc004996fae1553b267ae0752041ad
jonassmolarek <jonas.smolarek@agravis.de>
2019-07-19 12:18:08 +0000

    Änderungen_Ausdruck

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main {
      */
     public static void main(String[] args){
         // deklariere eine ganzzahlige Variable "birnen"
-		
+		 int birnen; 
 		// weise ihr den Wert drei zu
-
+        birnen = 3;
 		System.out.println("3 erwartet: " + birnen);
 		// addiere fuenf zu dem Wert
-
+        birnen += 5;
 		System.out.println("8 erwartet: " + birnen);
 		// deklariere eine Variable "aepfel" und weise ihr den Wert fuenf zu
-
+        int aepfel = 5;
 		System.out.println("5 erwartet: " + aepfel);
 		// subtrahiere zwei von dem Wert
-
+        aepfel -= 2;
 		System.out.println("3 erwartet: " + aepfel);
 
     }
